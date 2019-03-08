194d5e3a33c1a0278ac89108484b45c313dcb2c3
e2xist <e2xist@gmail.com>
2019-03-08 09:15:31 +0900

    minor update

--- app/src/main/java/kr/asv/apps/salarycalculator/activities/SettingsActivity.kt
+++ app/src/main/java/kr/asv/apps/salarycalculator/activities/SettingsActivity.kt
@@ -13,6 +13,7 @@ import android.support.v4.app.NavUtils
 import android.text.InputFilter
 import android.text.Spanned
 import android.view.MenuItem
+import kr.asv.androidutils.inputfilter.InputFilterDoubleMinMax
 import kr.asv.apps.salarycalculator.AppCompatPreferenceActivity
 import kr.asv.apps.salarycalculator.Services
 import kr.asv.shhtaxmanager.R
@@ -190,46 +191,6 @@ class SettingsActivity : AppCompatPreferenceActivity() {
         }
     }
 
-    class InputFilterDoubleMinMax : InputFilter {
-        private var min = 0.0
-        private var max: Double = 0.toDouble()
-
-        internal constructor(min: Int, max: Int) {
-            this.min = min.toDouble()
-            this.max = max.toDouble()
-        }
-
-        @Suppress("unused")
-        constructor(min: Double, max: Double) {
-            this.min = min
-            this.max = max
-        }
-
-        @Suppress("unused")
-        constructor(min: String, max: String) {
-            this.min = java.lang.Double.parseDouble(min)
-            this.max = java.lang.Double.parseDouble(max)
-        }
-
-        override fun filter(source: CharSequence, start: Int, end: Int, dest: Spanned, _start: Int, _end: Int): CharSequence? {
-            try {
-                val input = java.lang.Double.parseDouble(dest.toString() + source.toString())
-                if (isInRange(min, max, input))
-                    return null
-            } catch (ignored: NumberFormatException) {
-            }
-
-            return ""
-        }
-
-        private fun isInRange(a: Double, b: Double, c: Double): Boolean {
-            return if (b > a) {
-                c in a..b
-            } else {
-                c in b..a
-            }
-        }
-    }
 
     companion object {
 
