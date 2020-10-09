ada4f69a1e708b6df8f0a3ec7dc8ce78922525a0
e2xist <e2xist@gmail.com>
2020-10-10 05:34:40 +0900

    minor update

--- app/src/main/java/kr/asv/apps/salarycalculator/MainActivity.kt
+++ app/src/main/java/kr/asv/apps/salarycalculator/MainActivity.kt
@@ -2,16 +2,15 @@ package kr.asv.apps.salarycalculator
 
 import android.content.Context
 import android.os.Bundle
-import com.google.android.material.navigation.NavigationView
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.core.view.GravityCompat
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.app.AppCompatActivity
-import android.util.Log
 import android.view.MenuItem
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.ktx.Firebase
